b5a5cca65edf401866d0daaa1f93c5baae830e02
Vlad Makarenko <root@debian.esc.rzg.mpg.de>
2016-06-10 16:44:38 +0200

    support fileset concept of GPFS

--- seafile_keeper_ext/scripts/keeper_backup.sh
+++ seafile_keeper_ext/scripts/keeper_backup.sh
@@ -5,14 +5,15 @@ SEAFILE_DIR=/opt/seafile
 SEAFILE_LATEST_DIR=${SEAFILE_DIR}/seafile-server-latest
 BACKUP_POSTFIX="_orig"
 EXT_DIR=$(dirname $(dirname $(readlink -f $0)))
-PROPERTIES_FILE=${EXT_DIR}/keeper-qa.properties
-BACKUP_DIR=/keeper/test@lta02/backup/databases
+PROPERTIES_FILE=${SEAFILE_DIR}/keeper-qa.properties
+BACKUP_DIR=/keeper/test-fileset/db-backup
 
 PATH=$PATH:/usr/lpp/mmfs/bin
 export PATH
 TODAY=`date '+%Y%m%d'`
 GPFS_DEVICE="gpfs_keeper"
-GPFS_SNAPSHOT="mmbackupSnap${TODAY}"
+GPFS_FILESET="test-fileset"
+GPFS_SNAPSHOT="mmbackupSnap-test${TODAY}"
 
 # DEPENDENCY: for usage of nginx_dissite/nginx_ensite, install https://github.com/perusio/nginx_ensite
 HTTP_CONF=keeper.conf
@@ -113,24 +114,17 @@ function check_object_storage_integrity () {
 function asynchronous_backup () {
 	
     echo -e "Start asynchronous backup...\n"
-	# 0. Check for old snapshot
-	# If there is one, the last backup didn't finish -> something is wrong
-	local RESULT=$(mmlssnapshot $GPFS_DEVICE -s all)
-	if [[ ! "$RESULT" =~ "No snapshots in file system" ]]; then
-		# Old snapshot still exists, something is wrong
-		up_err_and_exit "Old snapshot(s) still existi(s)" 
-	fi
 	
-    # 2. Create filesystem snapshot
+    # 1. Create filesystem snapshot
 	echo "Create snapshot..."
-    mmcrsnapshot $GPFS_DEVICE $GPFS_SNAPSHOT
+    mmcrsnapshot $GPFS_DEVICE $GPFS_SNAPSHOT -j $GPFS_FILESET
     if [ $? -ne 0 ]; then
      # Could not create snapshot, something is wrong
-	    up_err_and_exit "Could not create snapshot $GPFS_SNAPSHOT" 
+	    up_err_and_exit "Could not create snapshot $GPFS_SNAPSHOT for fileset $GPFS_FILESET" 
     fi 
 	echo_green "OK"
 
-	# 3. TSM-Agent on lta03 will backup snapshot data asynchronously and delete snapshot after it is finished	
+	# 2. TSM-Agent on lta03 will backup snapshot data asynchronously and delete snapshot after it is finished	
     echo "Start remote backup..."
 	# TODO: generate log on remote !!!!
     #ssh lta03-mpdl "/bin/bash /opt/tivoli/tsm/client/ba/bin/do_mmbackup_vlad $GPFS_SNAPSHOT &"
@@ -145,7 +139,7 @@ function asynchronous_backup () {
 
 ##### START
 
-# check keeper environment
+###### CHECK 
 if [ ! -d "$BACKUP_DIR"  ]; then
 	err_and_exit "Cannot find backup directory: $BACKUP_DIR"
 fi
@@ -165,23 +159,38 @@ if [ ! $(type -P "nginx_ensite") ]; then
 	err_and_exit "Please install nginx_[en|dis]site: https://github.com/perusio/nginx_ensite"
 fi
 
+###### GPFS stuff
 if [ ! $(type -P "mmcrsnapshot") ]; then
 	err_and_exit "Cannot find GPFS executables: mmcrsnapshot"
 fi
-#TODO: check GPFS mount, probably preciser method! 
+#TODO: check GPFS mount, probably more precise method! 
 RESULT=$(mount -t gpfs)
 if [[ ! "$RESULT" =~ "/dev/gpfs_keeper on /keeper type gpfs" ]]; then
 	# Old snapshot still exists, something is wrong
 	err_and_exit "Cannot find mounted gpfs: $RESULT" 
 fi
 
+# Check for old GPFS snapshot
+# If there is one, the last backup didn't finish -> something is wrong
+RESULT=$(mmlssnapshot $GPFS_DEVICE -j $GPFS_FILESET)
+if [[ ! "$RESULT" =~ "No snapshots in file system" ]]; then
+    # The way to find at least one snapshot for the fileset. This is not implemented directly in mmlssnapshot, 
+    # see https://www.ibm.com/support/knowledgecenter/STXKQY_4.2.0/com.ibm.spectrum.scale.v4r2.adm.doc/bl1adm_mmlssnapshot.htm
+    RESULT=$(echo -e "$RESULT" | grep -vwE "^(Snapshots|Directory)" | rev | grep -Eo '^\s*[^ ]+' | rev) 
+    if [[ "$RESULT" =~ "$GPFS_FILESET" ]]; then
+        # Old snapshot still exists, something is wrong
+        err_and_exit "Old snapshots still exist" 
+    fi
+fi
+
 check_object_storage_integrity
+
+##### END CHECK
 
 shutdown_seafile
 
 echo -e "Backup seafile database...\n"
 for i in ccnet seafile seahub; do
-# TODO: gzip sql	
 #	mysqldump -h${__DB_HOST__} -u${__DB_USER__} -p${__DB_PASSWORD__} --verbose ${i}-db > ${BACKUP_DIR}/${i}-db.sql.`date +"%Y-%m-%d-%H-%M-%S"`
 	mysqldump -h${__DB_HOST__} -u${__DB_USER__} -p${__DB_PASSWORD__} --verbose ${i}-db | gzip > ${BACKUP_DIR}/`date +"%Y-%m-%d"`.${i}-db.sql.gz
 	if [ $? -ne 0  ]; then
