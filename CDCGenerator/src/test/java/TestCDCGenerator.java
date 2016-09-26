c95fdafdf9044d8c5afbd97416a18c91dea996f4
Projectarchive <drg.projectarchive@gmail.com>
2016-09-26 14:57:41 +0200

    1) +fixed JR design issues; 2)+ -g option: generate JasperReports file from JRXM

--- CDCGenerator/src/test/java/TestCDCGenerator.java
+++ CDCGenerator/src/test/java/TestCDCGenerator.java
@@ -13,17 +13,18 @@ import static org.junit.Assert.assertTrue;
 public class TestCDCGenerator {
 
     public static final String PATH_TO_CDC_PDF = "target/CDC.pdf";
+    public static final String PATH_TO_CDC_LONG_META_PDF = "target/CDC_long_meta.pdf";
 
-    @Ignore
     @Test
     public void testCDCGenerator() throws Exception {
         MainApp.main(new String[]{
                 "-i", "1",
-                "-aa", "\"Author1, Author2\"",
-                "-c", "\"keeper@mpdl.mpg.de",
+                "-aa", "\"Author1, Author2, Author3\"",
+                "-c", "keeper@mpdl.mpg.de",
                 "-d", "\"Description of the archived project\"",
                 "-t", "\"Library Title\"",
                 "-u", "\"https://keeper.mpdl.mpg.de/mylibrary\"",
+                //"-g",
                 "-h",
                 PATH_TO_CDC_PDF
 //                "Argument2"
@@ -33,17 +34,18 @@ public class TestCDCGenerator {
 
     }
 
+    @Ignore
     @Test
-    public void testCDCGenerator_Long_Variables() throws Exception {
+    public void testCDCGenerator_Long_Meta() throws Exception {
         MainApp.main(new String[]{
                 "-i", "1",
                 "-aa", "\"Author1, Author2, Author1, Author2, Author1, Author2, Author1, Author2, Author1, Author2, Author1, Author2, Author1, Author2, Author1, Author2, Author1, Author2, Author1, Author2, Author1, Author2, Author1, Author2, Author1, Author2, Author1, Author2, Author1, Author2, Author1, Author2, Author1, Author2, Author1, Author2, Author1, Author2, Author1, Author2, Author1, Author2\"",
-                "-c", "\"keeper@mpdl.mpg.de",
+                "-c", "keeper@mpdl.mpg.de",
                 "-d", "\"Description of the archived project Description of the archived project Description of the archived project Description of the archived project Description of the archived project Description of the archived project Description of the archived project Description of the archived project Description of the archived project Description of the archived project Description of the archived project Description of the archived project  Description of the archived project Description of the archived project Description of the archived project  Description of the archived project Description of the archived project Description of the archived project Description of the archived project Description of the archived project Description of the archived project Description of the archived project Description of the archived project\"",
                 "-t", "\"Very Long Title Longer As Wwo Lines Should be Cut Maximal Two Lines Plus Very Long Title Longer As Wwo Lines Should be Cut Maximal Two Lines Plus Very Long Title Longer As Wwo Lines Should be Cut Maximal Two Lines Plus Very Long Title Longer As Wwo Lines Should be Cut Maximal Two Lines Plus Very Long Title Longer As Wwowwwwwwwwwwwwwww Lines Should be Cut Maximal Two Lines Plus ...\"",
                 "-u", "\"https://keeper.mpdl.mpg.de/mylibrary\"",
                 "-h",
-                PATH_TO_CDC_PDF
+                PATH_TO_CDC_LONG_META_PDF
 //                "Argument2"
         });
         File f = new File(PATH_TO_CDC_PDF);
