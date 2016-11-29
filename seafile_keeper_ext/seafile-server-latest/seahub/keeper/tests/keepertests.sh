a2bf6aff726bf90b230462fdf4133e89311ad1c2
Vlad Makarenko <makarenko@mpdl.mpg.de>
2016-11-29 16:58:06 +0100

    +pytest env for keeper unit tests

---
+++ seafile_keeper_ext/seafile-server-latest/seahub/keeper/tests/keepertests.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+SEAFILE_DIR=/opt/seafile
+INSTALLPATH=${SEAFILE_DIR}/seafile-server-latest
+default_ccnet_conf_dir=${SEAFILE_DIR}/ccnet
+central_config_dir=${SEAFILE_DIR}/conf
+
+#get path of seafile.conf
+function read_seafile_data_dir () {
+    seafile_ini=${default_ccnet_conf_dir}/seafile.ini
+    if [[ ! -f ${seafile_ini} ]]; then
+        echo "${seafile_ini} not found. Now quit"
+        exit 1
+    fi
+    seafile_data_dir=$(cat "${seafile_ini}")
+    if [[ ! -d ${seafile_data_dir} ]]; then
+        echo "Your seafile server data directory \"${seafile_data_dir}\" is invalid or doesn't exits."
+        echo "Please check it first, or create this directory yourself."
+        echo ""
+        exit 1;
+    fi
+}
+
+read_seafile_data_dir;
+
+export CCNET_CONF_DIR=${default_ccnet_conf_dir}
+export SEAFILE_CONF_DIR=${seafile_data_dir}
+export SEAFILE_CENTRAL_CONF_DIR=${central_config_dir}
+
+export PYTHONPATH=${INSTALLPATH}/seafile/lib/python2.6/site-packages:${INSTALLPATH}/seafile/lib64/python2.6/site-packages:${INSTALLPATH}/seafile/lib/python2.7/site-packages:${INSTALLPATH}/seahub/thirdpart:$PYTHONPATH
+export PYTHONPATH=${INSTALLPATH}/seafile/lib/python2.7/site-packages:${INSTALLPATH}/seafile/lib64/python2.7/site-packages:$PYTHONPATH
+export PYTHONPATH=${INSTALLPATH}/seahub:$PYTHONPATH
+export PYTHONPATH="..:${PYTHONPATH}"
+
+
+
+OPTS=""
+while getopts ":" opt; do
+   OPTS="${OPTS}${OPTARG}" 
+done
+
+shift $(($OPTIND - 1))
+
+#if [ -n "$OPTS" ]; then
+    # always add verbose option
+    OPTS="-v${OPTS}"
+#fi
+
+pytest $OPTS $*    
+
+
