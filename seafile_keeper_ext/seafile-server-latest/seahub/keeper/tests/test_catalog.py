36e08d18f43e1171c2b921ae95420ecc83adac8c
Vlad Makarenko <makarenko@mpdl.mpg.de>
2016-12-09 15:38:57 +0100

    1)use seafile api for cdc_manager.py::get_user_name; 2)resolve unicode problems by catalog generation; 3) new pytests + fixtures

--- seafile_keeper_ext/seafile-server-latest/seahub/keeper/tests/test_catalog.py
+++ seafile_keeper_ext/seafile-server-latest/seahub/keeper/tests/test_catalog.py
@@ -1,29 +1,29 @@
 import pytest
 import json
-import cchardet
+# import cchardet
+
 
 from keeper.catalog.catalog_manager import is_in_mpg_ip_range, get_catalog, get_user_name
 
-@pytest.mark.skip
-def test_dummy():
-    pass
+# @pytest.mark.skip
+# def test_dummy():
+    # pass
 
 def test_is_in_mpg_ip_range():
     is_in_mpg = is_in_mpg_ip_range('192.129.78.1')
     assert is_in_mpg == False
 
 # @pytest.mark.skip
-def test_catalog(monkeypatch):
+def test_catalog():
     """Test catalog generation
     """
     catalog = get_catalog()
-    for r in catalog:
-        if 'owner_name' in r and  'Vlad' in r['owner_name']:
-            print r
-            print json.dumps(r, indent=4, ensure_ascii = False, sort_keys=True, separators=(',', ': '))#.encode('utf8')
-        # print json.dumps(catalog, ensure_ascii = False, encoding='latin-1', indent=4, sort_keys=True, separators=(',', ': '))
+    # for r in catalog:
+        # if 'owner_name' in r and  'Vlad' in r['owner_name']:
+            # print r
+            # print json.dumps(r, indent=4, ensure_ascii = False, sort_keys=True, separators=(',', ': '))#.encode('utf8')
+    print json.dumps(catalog, ensure_ascii = False, indent=4, sort_keys=True, separators=(',', ': '))
 
-def test_get_user_name():
-    uname = get_user_name('makarenko@mpdl.mpg.de')
-    print uname
+
+
 
