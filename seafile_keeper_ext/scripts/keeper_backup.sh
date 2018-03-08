f42e78b6b19e514170e78fb4adc839123a54ac8c
Vlad Makarenko <makarenko@mpdl.mpg.de>
2018-03-08 14:35:38 +0100

    1.+nginx.conf for keeper node 2.rsyslog.conf for syslog server 3.updated keeper_backup.sh script for cluster

--- seafile_keeper_ext/scripts/keeper_backup.sh
+++ seafile_keeper_ext/scripts/keeper_backup.sh
@@ -7,13 +7,16 @@ BACKUP_POSTFIX="_orig"
 
 PATH=$PATH:/usr/lpp/mmfs/bin
 export PATH
+TIMESTAMP=$(date +"%Y-%m-%d_%H:%M:%S")
 TODAY=`date '+%Y%m%d'`
+WEEKDAY=`date '+%u'`
+DAYOFMONTH=`date '+%d'`
 GPFS_DEVICE="app-keeper"
 GPFS_SNAPSHOT="mmbackupSnap${TODAY}"
-REMOVE_SNAPSHOT=0
+CLEANUP_SNAPSHOTS=1
 
 
-exec > >(tee /var/log/keeper/keeper_backup.`date '+%Y-%m-%d'`.log)
+exec > >(tee -a /var/log/keeper/keeper_backup.`date '+%Y-%m-%d'`.log)
 exec 2>&1 
 
 
@@ -31,19 +34,38 @@ function backup_databases () {
 
     echo -e "Backup seafile databases...\n"
 
-    # clean up old databases
-    #if [ "$(ls -A $DB_BACKUP_DIR)" ]; then
-        #echo "Clean ${DB_BACKUP_DIR}..."
-        #rm -v ${DB_BACKUP_DIR}/*
-        #[ $? -ne 0 ] && up_err_and_exit "Cannot clean up ${DB_BACKUP_DIR}"
-    #fi
-    local TIMESTAMP=$(date +"%Y-%m-%d_%H:%M:%S")
+    # clean up old database dumps
+    if [ "$(ls -A $DB_BACKUP_DIR)" ]; then
+        echo "Clean ${DB_BACKUP_DIR}..."
+        rm -v ${DB_BACKUP_DIR}/*
+        [ $? -ne 0 ] && err_and_exit "Cannot clean up ${DB_BACKUP_DIR}"
+    fi
     for i in ccnet seafile seahub keeper; do
         mysqldump -h${__DB_HOST__} -u${__DB_USER__} -p${__DB_PASSWORD__} -P${__DB_PORT__} --verbose ${i}-db | gzip > ${DB_BACKUP_DIR}/${TIMESTAMP}.${i}-db.sql.gz
-        [ $? -ne 0  ] && up_err_and_exit "Cannot dump ${i}-db"
+        [ $? -ne 0  ] && err_and_exit "Cannot dump ${i}-db"
     done
     echo_green "Databases backup is OK"
 
+}
+
+function cleanup_old_snapshots () {
+   
+    # Clean up old snapshots, leave at least LATEST_SN_COUNT latest snapshots 
+    LATEST_SN_COUNT=3
+    if [ $CLEANUP_SNAPSHOTS -eq 1 ]; then
+        echo -e "Clean up old snapshots...\n"
+        SNAPSHOTS=($(mmlssnapshot $GPFS_DEVICE | tail -n +3 | cut -d' ' -f1 | sort -r))
+        if [ -n "$SNAPSHOTS" ]; then
+            for SN in "${SNAPSHOTS[@]:$LATEST_SN_COUNT}"; do
+                CMD="mmdelsnapshot $GPFS_DEVICE $SN"
+                #echo $CMD
+                eval "$CMD"
+            done
+        else 
+            warn "No snapshots on $GPFS_DEVICE"
+        fi
+        echo_green "Clean up of old snapshots is OK"
+    fi
 }
 
 function backup_object_storage () {
@@ -58,7 +80,7 @@ function backup_object_storage () {
     # save current
     mmbackupconfig $GPFS_DEVICE -o $GPFS_BACKUP_CONF  
     if [ $? -ne 0 ]; then
-	    up_err_and_exit "Could not save GPFS backup config" 
+	    err_and_exit "Could not save GPFS backup config" 
     fi 
 	echo_green "OK"
 
@@ -67,34 +89,32 @@ function backup_object_storage () {
     mmcrsnapshot $GPFS_DEVICE $GPFS_SNAPSHOT 
     if [ $? -ne 0 ]; then
      # Could not create snapshot, something is wrong
-	    up_err_and_exit "Could not create snapshot $GPFS_SNAPSHOT" 
+	    err_and_exit "Could not create snapshot $GPFS_SNAPSHOT" 
     fi 
 	echo_green "OK"
 
     echo "Start TSM  backup..."
+    LOGLEVEL="-L 2"
+    export MMBACKUP_DSMC_BACKUP="-auditlogging=full -auditlogname=/var/log/mmbackup/tsm-auditlog-$DAYOFMONTH.log"
+    export MMBACKUP_DSMC_EXPIRE="-auditlogging=full -auditlogname=/var/log/mmbackup/tsm-auditlog-$DAYOFMONTH.log"
     # Full backup the first time:
-    mmbackup /keeper --scope inodespace --noquote -s /var/tmp -v -t full -B 1000 -L 6 -m 8 -S $GPFS_SNAPSHOT 
+    #mmbackup /keeper --scope inodespace --noquote -s /var/tmp -v -t full -B 1000 $LOGLEVEL -m 8 -S $GPFS_SNAPSHOT 
 
-    #if [ "$WEEKDAY" = 7 ]; then
-      ## Rebuild the shadow database on Sundays
-        #mmbackup /keeper --scope inodespace --noquote -s /var/tmp -v -q -t incremental -B 1000 -L 6 -m 8 -a 1 -S $GPFS_SNAPSHOT
-        #if [ $? -ne 0 ]; then
-            #up_err_and_exit "TSM backup failed" 
-        #fi 
-    #else
-      ## Normal incremental backup on other days
-        #mmbackup /keeper --scope inodespace --noquote -s /var/tmp -v -t incremental -B 1000 -L 6 -m 8 -a 1 -S $GPFS_SNAPSHOT 
-        #if [ $? -ne 0 ]; then
-            #up_err_and_exit "TSM backup failed" 
-        #fi 
-    #fi
-    #echo_green "OK"
+    # on Sunday
+    if [ "$WEEKDAY" = 7 ]; then
+      # Rebuild the shadow database on Sundays
+        mmbackup /keeper --noquote -s /var/tmp -v -q -t incremental -B 1000 $LOGLEVEL -m 8 -a 1 -S $GPFS_SNAPSHOT 
+        [ $? -ne 0 ] && warn "Incremental TSM backup with rebuild of shadow DB is failed" || echo_green "OK"
 
-    #if [ $REMOVE_SNAPSHOT -eq 1 ]; then
-        #mmdelsnapshot $GPFS_DEVICE $SNAPSHOT
-    #fi
+    else
+      # Normal incremental backup on other days
+        mmbackup /keeper --scope inodespace --noquote -s /var/tmp -v -t incremental -B 1000 $LOGLEVEL -m 8 -a 1 -S $GPFS_SNAPSHOT 
+        [ $? -ne 0 ] && warn "Incremental TSM backup is failed" || echo_green "OK"
+    fi
 
-    echo -e "Object Storage backup is OK\n"
+    cleanup_old_snapshots
+
+    echo_green "Object Storage backup is OK\n"
 		
 }
 
@@ -122,16 +142,14 @@ if [[ ! "$RESULT" =~ "${GPFS_DEVICE} on /keeper type gpfs" ]]; then
 	err_and_exit "Cannot find mounted gpfs: $RESULT" 
 fi
 
-# Check for old GPFS snapshot
-# If there is one, the last backup didn't finish -> something is wrong: ONLY WARNING!
-RESULT=$(mmlssnapshot $GPFS_DEVICE) 
-if [[ ! "$RESULT" =~ "No snapshots in file system" ]]; then
-    warn "Old snapshots still exist! Please clean them up!"
-fi
+# Exit if there is a snapshot today. 
+RESULT=$(mmlssnapshot ${GPFS_DEVICE} | tail -n +3 | cut -d' ' -f1 | grep ${GPFS_SNAPSHOT} -q)
+if [ $? -eq 0 ]; then
+    err_and_exit "Cannot create snapshot $GPFS_SNAPSHOT: the snapshot exists already." 
+fi 
+
 
 ##### END CHECK
-
-#shutdown_seafile
 
 backup_databases
 
