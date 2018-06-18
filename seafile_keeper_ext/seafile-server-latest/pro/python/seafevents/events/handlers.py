efb0d528709f37894e6d0f00b6c53d4948a8dc88
Vlad Makarenko <makarenko@mpdl.mpg.de>
2018-06-18 19:19:45 +0200

    correct imports

--- seafile_keeper_ext/seafile-server-latest/pro/python/seafevents/events/handlers.py
+++ seafile_keeper_ext/seafile-server-latest/pro/python/seafevents/events/handlers.py
@@ -4,10 +4,9 @@ import logging
 import logging.handlers
 import datetime
 
-from seaserv import get_related_users_by_repo, get_org_id_by_repo_id, \
-    get_related_users_by_org_repo, get_commit
-from .db import save_user_events, save_org_user_events, save_file_audit_event, \
-        save_file_update_event, save_perm_audit_event
+from seaserv import get_org_id_by_repo_id, seafile_api, get_commit
+from seafevents.events.db import save_user_events, save_org_user_events, \
+        save_file_audit_event, save_file_update_event, save_perm_audit_event
 
 from keeper.cdc.cdc_manager import generate_certificate_by_commit
 from keeper.catalog.catalog_manager import generate_catalog_entry_by_repo_id
