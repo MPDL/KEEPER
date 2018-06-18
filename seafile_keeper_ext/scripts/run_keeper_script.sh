86f0a4f973509165c20babe58cb075db5ce176fd
Vlad Makarenko <makarenko@mpdl.mpg.de>
2018-06-18 19:15:38 +0200

    1. +seafes and seahub to ENV variables for run_keeper_script.sh 2. SEAFILE_DIR in props for scripts

--- seafile_keeper_ext/scripts/run_keeper_script.sh
+++ seafile_keeper_ext/scripts/run_keeper_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SEAFILE_DIR=/opt/seafile
+SEAFILE_DIR=__SEAFILE_DIR__
 INSTALLPATH=${SEAFILE_DIR}/seafile-server-latest
 default_ccnet_conf_dir=${SEAFILE_DIR}/ccnet
 central_config_dir=${SEAFILE_DIR}/conf
@@ -31,6 +31,9 @@ read_seafile_data_dir;
 export CCNET_CONF_DIR=${default_ccnet_conf_dir}
 export SEAFILE_CONF_DIR=${seafile_data_dir}
 export SEAFILE_CENTRAL_CONF_DIR=${central_config_dir}
+export SEAFES_DIR=${INSTALLPATH}/pro/python/seafes
+export SEAHUB_DIR=${INSTALLPATH}/seahub
+
 
 export PYTHONPATH=${INSTALLPATH}/seafile/lib/python2.6/site-packages:${INSTALLPATH}/seafile/lib64/python2.6/site-packages:${INSTALLPATH}/seafile/lib/python2.7/site-packages:${INSTALLPATH}/seahub/thirdpart:$PYTHONPATH
 export PYTHONPATH=${INSTALLPATH}/seafile/lib/python2.7/site-packages:${INSTALLPATH}/seafile/lib64/python2.7/site-packages:$PYTHONPATH
