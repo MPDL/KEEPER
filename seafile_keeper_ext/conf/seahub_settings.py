5de1a37bdddc16f9ea3ff20d52c47c6a0d301450
Vlad Makarenko <makarenko@mpdl.mpg.de>
2018-06-25 15:18:09 +0200

    consolidated seahub_settings.py: all props moved from seahub/settings.py. See https://github.com/MPDL/KEEPER/issues/248

--- seafile_keeper_ext/conf/seahub_settings.py
+++ seafile_keeper_ext/conf/seahub_settings.py
@@ -1,5 +1,13 @@
 SECRET_KEY = __SECRET_KEY__
 
+##########################################################################
+#### Databases
+
+# keeper db
+KEEPER_DB_NAME = 'keeper-db'
+
+DATABASE_ROUTERS = ['keeper.dbrouter.DbRouter',]
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -14,7 +22,7 @@ DATABASES = {
     },
     'keeper': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'keeper-db',
+        'NAME': KEEPER_DB_NAME,
         'USER': '__DB_USER__',
         'PASSWORD': '__DB_PASSWORD__',
         'HOST': '__DB_HOST__',
@@ -25,7 +33,9 @@ DATABASES = {
     }
 }
 
-DATABASE_ROUTERS = ['keeper.dbrouter.DbRouter',]
+
+##########################################################################
+#### Caches
 
 CACHES = {
     'default': {
@@ -34,7 +44,9 @@ CACHES = {
     }
 }
 
-# Logging
+##########################################################################
+####  Logging
+
 import os
 LOG_DIR = os.environ.get('SEAHUB_LOG_DIR', '/tmp')
 LOGGING = {
@@ -106,46 +118,32 @@ LOGGING = {
         },
     }
 }
-# 'address': 'syslog-mpcdf.mpdl.mpg.de',
-# 'address': '/dev/log',
+
+# Debug mode
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+##########################################################################
+#### Web server <-> file server
+
+FILE_SERVER_ROOT = '__SERVICE_URL__/seafhttp'
+
+##########################################################################
+#### Cluster settings
+
 AVATAR_FILE_STORAGE = 'seahub.base.database_storage.DatabaseStorage'
 
 COMPRESS_CACHE_BACKEND = 'django.core.cache.backends.locmem.LocMemCache'
 
-FILE_SERVER_ROOT = '__SERVICE_URL__/seafhttp'
+##########################################################################
+#### User management options
 
 # Enalbe or disalbe registration on web. Default is `False`.
-# ENABLE_SIGNUP = True
+ENABLE_SIGNUP = True
 
-# PRO Options
-
-BRANDING_CSS = 'custom/keeper.css'
-
-LOGO_PATH = 'custom/__LOGO_IMG__'
-CUSTOM_LOGO_PATH = 'custom/__LOGO_IMG__'
-
-FAVICON_PATH = 'img/favicon.png'
-CUSTOM_FAVICON_PATH = 'img/favicon.png'
-
-# Whether to show the used traffic in user's profile popup dialog. Default is True
-SHOW_TRAFFIC = True
-
-# Allow administrator to view user's file in UNENCRYPTED libraries
-# through Libraries page in System Admin. Default is False.
-ENABLE_SYS_ADMIN_VIEW_REPO = False
-
-DESKTOP_CUSTOM_LOGO = 'custom/keeper-client-logo.png'
-DESKTOP_CUSTOM_BRAND = 'KEEPER'
-
-# MPDL EMAIL settings
-EMAIL_USE_TLS = True
-EMAIL_HOST = '__EMAIL_HOST__'        # smpt server
-EMAIL_HOST_USER = '__EMAIL_HOST_USER__'    # username and domain
-EMAIL_HOST_PASSWORD = '__EMAIL_HOST_PASSWORD__'    # password
-EMAIL_PORT = '__EMAIL_PORT__'
-###### DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-DEFAULT_FROM_EMAIL = '__DEFAULT_FROM_EMAIL__'
-SERVER_EMAIL = '__SERVER_EMAIL__'
+# Activate or deactivate user when registration complete. Default is `True`.
+# If set to `False`, new users need to be activated by admin in admin panel.
+ACTIVATE_AFTER_REGISTRATION = False
 
 # Whether to send email when a system admin adding a new member. Default is `True`.
 SEND_EMAIL_ON_ADDING_SYSTEM_MEMBER = True
@@ -153,24 +151,164 @@ SEND_EMAIL_ON_ADDING_SYSTEM_MEMBER = True
 # Whether to send email when a system admin resetting a user's password. Default is `True`.
 SEND_EMAIL_ON_RESETTING_USER_PASSWD = True
 
-# Multiple Organization/Institution User Management, see http://manual.seafile.com/deploy_pro/multi_institutions.html
-MULTI_INSTITUTION = True
+# Send system admin notify email when user registration is complete. Default is `False`.
+NOTIFY_ADMIN_AFTER_REGISTRATION = True
 
-EXTRA_MIDDLEWARE_CLASSES = (
-    'seahub.institutions.middleware.InstitutionMiddleware',
-    'shibboleth.middleware.ShibbolethRemoteUserMiddleware',
-)
+# Remember days for login. Default is 7
+LOGIN_REMEMBER_DAYS = 7
+
+# Attempt limit before showing a captcha when login.
+LOGIN_ATTEMPT_LIMIT = 3
+
+# deactivate user account when login attempts exceed limit
+# Since version 5.1.2 or pro 5.1.3
+FREEZE_USER_ON_LOGIN_FAILED = False
+
+# mininum length for user's password
+USER_PASSWORD_MIN_LENGTH = 6
+
+# LEVEL based on four types of input:
+# num, upper letter, lower letter, other symbols
+# '3' means password must have at least 3 types of the above.
+USER_PASSWORD_STRENGTH_LEVEL = 3
+
+# default False, only check USER_PASSWORD_MIN_LENGTH
+# when True, check password strength level, STRONG(or above) is allowed
+USER_STRONG_PASSWORD_REQUIRED = True
+
+# Force user to change password when admin add/reset a user.
+# Added in 5.1.1, deafults to True.
+FORCE_PASSWORD_CHANGE = True
+
+# Age of cookie, in seconds (default: 2 weeks).
+SESSION_COOKIE_AGE = 60 * 60 * 24 * 7 * 2
+
+# Whether a user's session cookie expires when the Web browser is closed.
+SESSION_EXPIRE_AT_BROWSER_CLOSE = False
+
+# Whether to save the session data on every request. Default is `False`
+SESSION_SAVE_EVERY_REQUEST = False
+
+# Whether enable personal wiki and group wiki. Default is `False`
+# Since 6.1.0 CE
+ENABLE_WIKI = True
+
+##########################################################################
+#### Repo snapshot label feature
+
+# Turn on this option to let users to add a label to a library snapshot.
+# Default is `False`
+ENABLE_REPO_SNAPSHOT_LABEL = True
+
+
+##########################################################################
+#### Library options
+
+# mininum length for password of encrypted library
+REPO_PASSWORD_MIN_LENGTH = 8
+
+# mininum length for password for share link (since version 4.4)
+SHARE_LINK_PASSWORD_MIN_LENGTH = 8
+
+# Disable sync with any folder. Default is `False`
+# NOTE: since version 4.2.4
+DISABLE_SYNC_WITH_ANY_FOLDER = True
+
+# Enable or disable library history setting
+ENABLE_REPO_HISTORY_SETTING = True
+
+# Enable or disable normal user to create organization libraries
+# Since version 5.0.5
+ENABLE_USER_CREATE_ORG_REPO = False
+
+# Enable or disable user share library to any group
+# Since version 6.2.0
+ENABLE_SHARE_TO_ALL_GROUPS = True
+
+
+##########################################################################
+#### Options for online file preview
+
+# Whether to use pdf.js to view pdf files online. Default is `True`,  you can turn it off.
+# NOTE: since version 1.4.
+USE_PDFJS = True
+
+# Online preview maximum file size, defaults to 30M.
+# Note, this option controls files that can be previewed online, like pictures, txt, pdf.
+# In pro edition, for preview doc/ppt/excel/pdf, there is another option `max-size`
+# in seafevents.conf that controls the limit of files that can be previewed.
+FILE_PREVIEW_MAX_SIZE = 30 * 1024 * 1024
+
+# Extensions of previewed text files.
+# NOTE: since version 6.1.1
+TEXT_PREVIEW_EXT = """ac, am, bat, c, cc, cmake, cpp, cs, css, diff, el, h, html,
+htm, java, js, json, less, make, org, php, pl, properties, py, rb,
+scala, script, sh, sql, txt, text, tex, vi, vim, xhtml, xml, log, csv,
+groovy, rst, patch, go"""
+
+# Enable or disable thumbnails
+# NOTE: since version 4.0.2
+ENABLE_THUMBNAIL = True
+
+# Seafile only generates thumbnails for images smaller than the following size.
+THUMBNAIL_IMAGE_SIZE_LIMIT = 30 # MB
+
+# Enable or disable thumbnail for video. ffmpeg and moviepy should be installed first.
+# For details, please refer to https://manual.seafile.com/deploy/video_thumbnails.html
+# NOTE: since version 6.1
+ENABLE_VIDEO_THUMBNAIL = False
+
+# Use the frame at 5 second as thumbnail
+THUMBNAIL_VIDEO_FRAME_TIME = 5
+
+# Absolute filesystem path to the directory that will hold thumbnail files.
+THUMBNAIL_ROOT = '__SEAFILE_DIR__/seahub-data/thumbnail/thumb/'
+
+# Default size for picture preview. Enlarge this size can improve the preview quality.
+# NOTE: since version 6.1.1
+THUMBNAIL_SIZE_FOR_ORIGINAL = 1024
+
+##########################################################################
+#### Cloud Mode
+
+# Enable cloude mode and hide `Organization` tab.
+CLOUD_MODE = False
+
+# Disable global address book
+ENABLE_GLOBAL_ADDRESSBOOK = True
+
+##########################################################################
+#### MULTI_TENANCY
+
+# MULTI_TENANCY = True
+
+ORG_MEMBER_QUOTA_ENABLED = True
+
+##########################################################################
+#### External authentication
+
+# Enable authentication with ADFS
+# Default is False
+# Since 6.0.9
+ENABLE_ADFS_LOGIN = False
+
+# Enable authentication wit Kerberos
+# Default is False
+ENABLE_KRB5_LOGIN = False
+
+# Enable authentication with Shibboleth
+# Default is False
+ENABLE_SHIB_LOGIN = __ENABLE_SHIB_LOGIN__
 
 EXTRA_AUTHENTICATION_BACKENDS = (
     'shibboleth.backends.ShibbolethRemoteUserBackend',
 )
 
-ENABLE_SHIB_LOGIN = __ENABLE_SHIB_LOGIN__
-
 SHIBBOLETH_ATTRIBUTE_MAP = {
-            # Change eppn to mail if you use mail attribute for REMOTE_USER
-                "eppn": (False, "username"),
+    # Change eppn to mail if you use mail attribute for REMOTE_USER
+    "eppn": (False, "username"),
 }
+
 SHIBBOLETH_ATTRIBUTE_MAP = {
     "eppn": (False, "username"),
     "givenName": (False, "givenname"),
@@ -232,47 +370,138 @@ ENABLED_ROLE_PERMISSIONS = {
     },
 }
 
-ENABLE_SETTINGS_VIA_WEB = True
+##########################################################################
+#### Other
 
+# Disable settings via Web interface in system admin->settings
+# Default is True
+# Since 5.1.3
+ENABLE_SETTINGS_VIA_WEB = False
+
+# Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'Europe/Berlin'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+# Default language for sending emails.
+LANGUAGE_CODE = 'en'
+
+# Set this to your website/company's name. This is contained in email notifications and welcome message when user login for the first time.
+SITE_NAME = 'KEEPER'
+
+# Browser tab's title
+SITE_TITLE = 'KEEPER'
+
+# If you don't want to run seahub website on your site's root path, set this option to your preferred path.
+# e.g. setting it to '/seahub/' would run seahub on http://example.com/seahub/.
+SITE_ROOT = '/'
+
+# Max number of files when user upload file/folder.
+# Since version 6.0.4
+MAX_NUMBER_OF_FILES_FOR_FILEUPLOAD = 500
+
+# Control the language that send email. Default to user's current language.
+# Since version 6.1.1
+SHARE_LINK_EMAIL_LANGUAGE = ''
+
+# Interval for browser requests unread notifications
+# Since PRO 6.1.4 or CE 6.1.2
+UNREAD_NOTIFICATIONS_REQUEST_INTERVAL = 3 * 60 # seconds
+
+# enable 'upload folder' or not
 ENABLE_UPLOAD_FOLDER = True
 
-# Turn on this option to let users to add a label to a library snapshot.
-# Default is `False`
-ENABLE_REPO_SNAPSHOT_LABEL = True
-
-
-# Enable or disable library history setting
-ENABLE_REPO_HISTORY_SETTING = False
-
-# Enable or disable user share library to any group
-# Since version 6.2.0
-ENABLE_SHARE_TO_ALL_GROUPS = True
-
-
-####################################################################
-# KEEPER specific settings
-
-
-# Keeper root dir
-SEAFILE_DIR = '__SEAFILE_DIR__'
-
-
-ARCHIVE_METADATA_TARGET = 'archive-metadata.md'
-KEEPER_DEFAULT_LIBRARY = 'Keeper Default Library'
-KEEPER_DB_NAME = 'keeper-db'
-
-TEST_SERVER='__TEST_SERVER__'
-TEST_SERVER_ADMIN='__TEST_SERVER_ADMIN__'
+# Override language settings
+LANGUAGES = (
+    ('de', 'Deutsch'),
+    ('en', 'English'),
+)
 
 
 
-# Enable cloude mode and hide `Organization` tab.
-CLOUD_MODE = True
+##########################################################################
+#### Pro edition only options
 
-# use  pdf.js instead of pdf2html
-USE_PDFJS = True
+# Whether to show the used traffic in user's profile popup dialog. Default is True
+SHOW_TRAFFIC = True
 
-# TEST_SERVER_PASSWORD="__TEST_SERVER_ADMIN_PASSWORD__"
+# Allow administrator to view user's file in UNENCRYPTED libraries
+# through Libraries page in System Admin. Default is False.
+ENABLE_SYS_ADMIN_VIEW_REPO = False
+
+# For un-login users, providing an email before downloading or uploading on shared link page.
+# Since version 5.1.4
+ENABLE_SHARE_LINK_AUDIT = False
+
+# Check virus after upload files to shared upload links. Defaults to `False`.
+# Since version 6.0
+ENABLE_UPLOAD_LINK_VIRUS_CHECK = True
+
+# Enable system admin add T&C, all users need to accept terms before using. Defaults to `False`.
+# Since version 6.0
+ENABLE_TERMS_AND_CONDITIONS = True
+
+# Enable two factor authentication for accounts. Defaults to `False`.
+# Since version 6.0
+ENABLE_TWO_FACTOR_AUTH = False
+
+# Enable user select a template when he/she creates library.
+# When user select a template, Seafile will create folders releated to the pattern automaticly.
+# Since version 6.0
+# LIBRARY_TEMPLATES = {
+    # 'Technology': ['/Develop/Python', '/Test'],
+    # 'Finance': ['/Current assets', '/Fixed assets/Computer']
+# }
+
+# Send email to these email addresses when a virus is detected.
+# This list can be any valid email address, not necessarily the emails of Seafile user.
+# Since version 6.0.8
+VIRUS_SCAN_NOTIFY_LIST = ['__SERVER_EMAIL__']
+
+##########################################################################
+#### KEEPER branding settings
+
+BRANDING_CSS = 'custom/keeper.css'
+
+LOGO_PATH = 'custom/__LOGO_IMG__'
+CUSTOM_LOGO_PATH = 'custom/__LOGO_IMG__'
+LOGO_WIDTH = 140
+LOGO_HEIGHT = 40
+
+FAVICON_PATH = 'img/favicon.png'
+CUSTOM_FAVICON_PATH = 'img/favicon.png'
+
+DESKTOP_CUSTOM_LOGO = 'custom/keeper-client-logo.png'
+DESKTOP_CUSTOM_BRAND = 'KEEPER'
+
+# MPDL EMAIL settings
+
+##########################################################################
+#### Email settings
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = '__EMAIL_HOST__'        # smpt server
+EMAIL_HOST_USER = '__EMAIL_HOST_USER__'    # username and domain
+EMAIL_HOST_PASSWORD = '__EMAIL_HOST_PASSWORD__'    # password
+EMAIL_PORT = '__EMAIL_PORT__'
+###### DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = '__DEFAULT_FROM_EMAIL__'
+SERVER_EMAIL = '__SERVER_EMAIL__'
+
+##########################################################################
+#### Multiple Organization/Institution User Management, see http://manual.seafile.com/deploy_pro/multi_institutions.html
+
+MULTI_INSTITUTION = True
+
+EXTRA_MIDDLEWARE_CLASSES = (
+    'seahub.institutions.middleware.InstitutionMiddleware',
+    'shibboleth.middleware.ShibbolethRemoteUserMiddleware',
+)
+
 
 # Settings for background node
 # OFFICE_CONVERTOR_NODE = __IS_OFFICE_CONVERTOR_NODE__
@@ -305,3 +534,67 @@ ENABLE_OFFICE_WEB_APP_EDIT = True
 # types of files should be editable through LibreOffice Online
 OFFICE_WEB_APP_EDIT_FILE_EXTENSION = ('ods', 'xls', 'xlsb', 'xlsm', 'xlsx','ppsx', 'ppt',
     'pptm', 'pptx', 'doc', 'docm', 'docx', 'odt', 'odp')
+
+
+##########################################################################
+####  KEEPER specific settings
+
+# add keeper to django apps list
+INSTALLED_APPS = (
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'registration',
+    'captcha',
+    'compressor',
+    'statici18n',
+    'constance',
+    'constance.backends.database',
+    'post_office',
+    'termsandconditions',
+
+    'seahub.api2',
+    'seahub.avatar',
+    'seahub.base',
+    'seahub.contacts',
+    'seahub.institutions',
+    'seahub.invitations',
+    'seahub.wiki',
+    'seahub.group',
+    'seahub.notifications',
+    'seahub.options',
+    'seahub.onlyoffice',
+    'seahub.profile',
+    'seahub.share',
+    'seahub.help',
+    'seahub.thumbnail',
+    'seahub.password_session',
+    'seahub.admin_log',
+    'seahub.wopi',
+    'seahub.tags',
+    'seahub.revision_tag',
+    'seahub.two_factor',
+    'seahub.role_permissions',
+    'seahub.trusted_ip',
+
+    'keeper',
+
+)
+
+# Keeper root dir
+SEAFILE_DIR = '__SEAFILE_DIR__'
+
+
+# default filename for CDC metadata
+ARCHIVE_METADATA_TARGET = 'archive-metadata.md'
+
+# default KEEPER library name
+KEEPER_DEFAULT_LIBRARY = 'Keeper Default Library'
+
+# TEST_SERVER='__TEST_SERVER__'
+# TEST_SERVER_PASSWORD="__TEST_SERVER_ADMIN_PASSWORD__"
+# TEST_SERVER_ADMIN='__TEST_SERVER_ADMIN__'
+
+
