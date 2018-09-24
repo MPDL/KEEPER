6f85bafe93360b21156f505646b1d936f8490c17
Vlad Makarenko <makarenko@mpdl.mpg.file>
2018-09-24 13:48:27 +0000

    add KEEPER DEFAULT LIBRARY to admin for first call of a libray creation (CDC init workflow)

--- seafile_keeper_ext/seafile-server-latest/seahub/keeper/default_library_manager.py
+++ seafile_keeper_ext/seafile-server-latest/seahub/keeper/default_library_manager.py
@@ -1,17 +1,36 @@
 import os
-import sys
 
 import logging
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "seahub.settings") 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "seahub.settings")
 
 from seaserv import seafile_api
-from seahub.settings import SERVER_EMAIL, KEEPER_DEFAULT_LIBRARY
+from seahub.settings import SERVER_EMAIL, KEEPER_DEFAULT_LIBRARY, SEAFILE_DIR, ARCHIVE_METADATA_TARGET
 
-DEBUG = False 
+DEBUG = False
 
 if DEBUG:
     logging.basicConfig(level=logging.INFO)
+
+def create_keeper_default_library():
+    """ create keeper default library
+        and put CDC files into it
+    """
+    # create repo
+    repo_id = seafile_api.create_repo(KEEPER_DEFAULT_LIBRARY, "The library is used for keeper CDC generation", SERVER_EMAIL, passwd=None)
+    if repo_id:
+        # add CDC files
+        dir_path = os.path.join(SEAFILE_DIR, 'seafile-server-latest', 'seahub', 'keeper', 'cdc')
+        for fn in (ARCHIVE_METADATA_TARGET, 'Cared-Data-Certificate-HowTo.pdf'):
+            path = os.path.join(dir_path, fn)
+            if os.path.isfile(path):
+                seafile_api.post_file(repo_id, path, "/", fn, SERVER_EMAIL)
+            else:
+                logging.error("File {} does not exist.".format(path))
+                return None
+        return repo_id
+    else:
+        return None
 
 def get_keeper_default_library():
     try:
@@ -19,14 +38,19 @@ def get_keeper_default_library():
         from_repos = [r for r in from_repos if r.name == KEEPER_DEFAULT_LIBRARY]
         if from_repos:
             from_repo_id = from_repos[0].id
-            return { 'repo_id': from_repo_id, 'dirents': seafile_api.list_dir_by_path(from_repo_id, '/') }
         else:
-            raise Exception("Cannot find KEEPER_DEFAULT_LIBRARY repo in admin libraries, please install!")
+            logging.info("Cannot find KEEPER_DEFAULT_LIBRARY repo in admin libraries, trying to create...")
+            from_repo_id = create_keeper_default_library()
+            if from_repo_id is None:
+                raise Exception("Cannot create KEEPER_DEFAULT_LIBRARY repo in admin libraries, please check!")
+            else:
+                logging.info("KEEPER_DEFAULT_LIBRARY has been successfully created!")
+        return { 'repo_id': from_repo_id, 'dirents': seafile_api.list_dir_by_path(from_repo_id, '/') }
     except Exception as err:
         logging.error("Cannot find KEEPER_DEFAULT_LIBRARY dirents, err: " + str(err))
         return None
 
- 
+
 
 def copy_keeper_default_library(to_repo_id):
     logging.info("Add KEEPER_DEFAULT_LIBRARY files to the repo %s..." % to_repo_id)
@@ -43,5 +67,5 @@ def copy_keeper_default_library(to_repo_id):
         logging.error("Cannot copy KEEPER_DEFAULT_LIBRARY, err: " + str(err))
 
 
-if DEBUG: 
-    copy_keeper_default_library('d1fe0133-25f2-4ce3-b5c1-055f1ec9716d') 
+if DEBUG:
+    copy_keeper_default_library('d1fe0133-25f2-4ce3-b5c1-055f1ec9716d')
