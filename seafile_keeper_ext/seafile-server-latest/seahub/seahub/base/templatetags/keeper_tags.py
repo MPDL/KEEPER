38d363482269630c79c231ecdfffe13666b8f664
Vlad Makarenko <makarenko@mpdl.mpg.de>
2018-11-07 11:31:47 +0100

    fix https://github.com/MPDL/KEEPER/issues/285: E-mail "New notice on Keeper" -> no text in field "Message". + django templates filter refactor cds notifaction  messages with get_value_from_json filter

---
+++ seafile_keeper_ext/seafile-server-latest/seahub/seahub/base/templatetags/keeper_tags.py
@@ -0,0 +1,17 @@
+from django import template
+from django.utils.html import escape
+from django.utils.encoding import force_unicode
+import json
+
+register = template.Library()
+
+@register.filter(name='get_value_from_json')
+def get_value_from_json(value, key):
+    """
+    get value by key from json string
+    """
+    json_dict = json.loads(value)
+    result = ''
+    if key in json_dict:
+        result = force_unicode(escape(json_dict[key]))
+    return result
