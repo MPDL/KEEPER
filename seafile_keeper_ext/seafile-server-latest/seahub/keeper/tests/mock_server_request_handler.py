62ae5e6463fcd1ac021873e85fb02d7d11de705c
yli18 <y.li@mpdl.mpg.de>
2019-03-06 15:12:53 +0000

    unit test

--- seafile_keeper_ext/seafile-server-latest/seahub/keeper/tests/mock_server_request_handler.py
+++ seafile_keeper_ext/seafile-server-latest/seahub/keeper/tests/mock_server_request_handler.py
@@ -4,23 +4,32 @@ import json
 import re
 import socket
 from threading import Thread
+from cgi import parse_header, parse_multipart
+from urlparse import parse_qs
 
 # Third-party imports...
 import requests
 
 
 class MockServerRequestHandler(BaseHTTPRequestHandler):
+
     USERS_PATTERN = re.compile(r'/certifyData') 
 
     def do_POST(self):
         if re.search(self.USERS_PATTERN, self.path):
-            self.send_response(requests.codes.ok)
-            self.send_header('Content-Type', 'application/json; charset=utf-8')
-            self.end_headers()
+            self.data_string = self.rfile.read(int(self.headers['Content-Length']))
+            #data = json.loads(self.data_string)
+            print(self.data_string)
+            if self.data_string != '{}':
+                self.send_response(requests.codes.ok)
+                self.send_header('Content-Type', 'application/json; charset=utf-8')
+                self.end_headers()
 
-            # Add response content.
-            response_content = json.dumps({"msg":"Transaction succeeded","txReceipt":{"transactionHash":"0x3d302773bbebd1a9d44ff96784fae2082df483ecf50a695e70cddc9d54da5e57"}})
-            self.wfile.write(response_content.encode('utf-8'))
+                # Add response content.
+                response_content = json.dumps({"msg":"Transaction succeeded","txReceipt":{"transactionHash":"0x3d302773bbebd1a9d44ff96784fae2082df483ecf50a695e70cddc9d54da5e57"}})
+                self.wfile.write(response_content.encode('utf-8'))
+            else:
+                return None    
 
 
 def get_free_port():
--- seafile_keeper_ext/seafile-server-latest/seahub/keeper/tests/test_bloxberg.py
+++ seafile_keeper_ext/seafile-server-latest/seahub/keeper/tests/test_bloxberg.py
@@ -10,7 +10,8 @@ from seahub.test_utils import BaseTestCase
 
 from urllib import quote
 
-import seahub.api2.views_keeper
+import seahub.api2.views_keeper as views_keeper
+import keeper.bloxberg.bloxberg_manager as bloxberg_manager
 from mock_server_request_handler import get_free_port, start_mock_server
 import mock
 import json
@@ -32,7 +33,7 @@ class BloxbergTest(BaseTestCase):
 
   def tearDown(self):
     self.remove_user(self.user.username)
-
+  
   def test_certify_succeed(self): 
 
     """Test certify api
@@ -60,5 +61,70 @@ class BloxbergTest(BaseTestCase):
       print(json_resp['msg'])
       self.assertTrue(json_resp['txReceipt']['transactionHash'] is not None)
       print('transaction_hash: {0}'.format(json_resp['txReceipt']['transactionHash']))
+    
+  def test_request_bloxberg_succeed(self):
+
+    certify_payload = {
+      'certifyVariables': {
+        'checksum': 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855', 
+        'authorName': 'paipaibear90', 
+        'timestampString': '1551692787.55'
+      }
+    }
+
+    mock_users_url = 'http://localhost:{port}/certifyData'.format(port=self.mock_server_port)
+
+    # Patch URL so that the service uses the mock server URL instead of the real URL.
+    with mock.patch.dict('seahub.api2.views_keeper.__dict__', {'URL': mock_users_url}):
+      resp = views_keeper.request_bloxberg(certify_payload)
+      json_resp = resp.json()
+      self.assertEqual(200, resp.status_code)
+      self.assertEqual("Transaction succeeded", json_resp['msg'])
+      print(json_resp['msg'])
+      self.assertTrue(json_resp['txReceipt']['transactionHash'] is not None)
+      print('transaction_hash: {0}'.format(json_resp['txReceipt']['transactionHash']))      
+
+
+  def test_request_bloxberg_failed(self):
+
+    certify_payload = {
+    }
+
+    mock_users_url = 'http://localhost:{port}/certifyData'.format(port=self.mock_server_port)
+
+    # Patch URL so that the service uses the mock server URL instead of the real URL.
+    with mock.patch.dict('seahub.api2.views_keeper.__dict__', {'URL': mock_users_url}):
+      resp = views_keeper.request_bloxberg(certify_payload)
+      self.assertTrue(resp is None)  
+  
+
+  def test_hash_file_succeed(self):
+    try:
+      file_hash = bloxberg_manager.hash_file(self.repo, self.file)
+      print('file_hash: {0}'.format(file_hash))
+    except AttributeError as e:
+      print(str(e))
+
+  def test_hash_file_failed(self):
+    try:
+      file_hash = bloxberg_manager.hash_file(self.repo, self.file)
+      print('file_hash: {0}'.format(file_hash))
+    except AttributeError as e:
+      print(str(e))
+      self.assertTrue("'NoneType' object has no attribute 'get_stream'" in str(e))
+
+  def test_commit_id(self):
+    self.login_as(self.user)
+    commit_id = bloxberg_manager.get_commit_root_id(self.repo.repo_id)
+    print(commit_id)
+    self.assertTrue(commit_id)
+
+  def test_get_file_by_path(self):
+    self.login_as(self.user) 
+    try:
+      file_dir = bloxberg_manager.hash_file(self.repo.repo_id, self.file, EMAIL)
+      print(file_dir) 
+    except Exception as e:
+      print(str(e))
 
 
