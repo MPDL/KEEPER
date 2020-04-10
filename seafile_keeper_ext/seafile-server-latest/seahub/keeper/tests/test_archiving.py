92d185dbebb37d4624454080a46213d032295f84
Vlad Makarenko <makarenko@mpdl.mpg.de>
2020-04-10 18:12:09 +0000

    1. migration of test to seafile 7.1.1 2. better keeper arciving tests, beta

--- seafile_keeper_ext/seafile-server-latest/seahub/keeper/tests/test_archiving.py
+++ seafile_keeper_ext/seafile-server-latest/seahub/keeper/tests/test_archiving.py
@@ -1,4 +1,5 @@
 import pytest
+import unittest
 
 import seahub.base
 import django
@@ -13,18 +14,18 @@ import tempfile
 from seaserv import seafile_api
 from seahub.settings import SERVER_EMAIL
 
-from keepertestbase import create_tmp_user_with_profile, create_tmp_user, create_tmp_repo
+from http import HTTPStatus
 
-import keeper.archiving.archiving_manager as arch_mgr
+from .keepertestbase import create_tmp_user_with_profile, create_tmp_user, create_tmp_repo
 
 from datetime import datetime
 
-from seafevents.keeper_archiving import KeeperArchiving
-from seafevents.keeper_archiving.task_manager import task_manager, Worker
-from seafevents.keeper_archiving.config import get_keeper_archiving_conf
-from seafevents.utils import get_config
+# from seafevents.keeper_archiving import KeeperArchiving
+# from seafevents.keeper_archiving.task_manager import task_manager, Worker
+# from seafevents.keeper_archiving.config import get_keeper_archiving_conf
+# from seafevents.utils import get_config
 from keeper.utils import query_keeper_archiving_status,\
-        add_keeper_archiving_task, get_keeper_archiving_quota
+        add_keeper_archiving_task, check_keeper_repo_archiving_status
 from time import sleep
 
 import json
@@ -32,146 +33,247 @@ from seahub.notifications.models import UserNotification
 
 from seafevents.keeper_archiving import task_manager as arch_task_mgr
 
-@pytest.mark.skip
-def test_tmp_repo_archiving(create_tmp_repo):
-    """TODO: Docstring for test_archiving.
+#@pytest.mark.skip
 
-    :create_tmp_repo: TODO
-    :returns: TODO
+class TestKeeperArchiving(unittest.TestCase):
 
-    """
-    repo = create_tmp_repo
-    assert not repo is None
+    @classmethod
+    def setUpClass(cls):
+        cls.user = next(create_tmp_user())
+        cls.repo = next(create_tmp_repo(cls.user))
 
-    f = tempfile.NamedTemporaryFile()
-    f.write('Text')
-    f.flush()
-    # os.chmod(f.name, 0666)
+    @classmethod
+    def tearDownClass(cls):
+        #TODO: delete repo & user
+        pass
 
-    seafile_api.post_file(repo.id, f.name, "/", "some_file_in_root.txt", SERVER_EMAIL)
-    f.close()
+    def test_check_keeper_repo_archiving_status(self):
+        """TODO: Docstring for test_archiving.
+        """
+
+        repo_id = self.repo.id
+
+        # not archived
+        # errors
+        # resp = check_keeper_repo_archiving_status('fake_repo_id', SERVER_EMAIL, 'is_snapshot_archived')
+        # assert 'Message: invalid repo id fake_repo_id.' in resp['error']
+        #
+        # resp = check_keeper_repo_archiving_status(repo_id, "fake_owner", 'is_snapshot_archived')
+        # assert 'Cannot archive. User is not the owner of the library.' == resp['error']
+        #
+        # resp = check_keeper_repo_archiving_status(repo_id, SERVER_EMAIL, 'fake_action')
+        # assert resp['status'] == 'ERROR'
+        # assert resp['error'] == 'Unknown action: fake_action'
+        #
+        # # is_snapshot_archived
+        # resp = check_keeper_repo_archiving_status(repo_id, SERVER_EMAIL, 'is_snapshot_archived')
+        # assert resp['action'] == 'is_snapshot_archived'
+        # assert resp['is_snapshot_archived'] == 'false'
+        #
+        # # get_quota
+        # resp = check_keeper_repo_archiving_status(repo_id, SERVER_EMAIL, 'get_quota')
+        # print(resp)
+        # assert resp['curr_ver'] == 0
+        # assert resp['remains'] == 5
+        #
+        # # is_repo_too_big
+        # resp = check_keeper_repo_archiving_status(repo_id, SERVER_EMAIL, 'is_repo_too_big')
+        # print(resp)
+        # assert resp['is_repo_too_big'] == 'false'
+
+        with open("data/archive-metadata.md", 'r') as f:
+            seafile_api.post_file(repo_id, os.path.abspath(f.name), "/", "archive-metadata.md", self.user)
+        with open("data/sample.png", 'r') as f:
+            seafile_api.post_file(repo_id, os.path.abspath(f.name), "/", "sample.png", self.user)
+
+        sleep(5)
+
+        resp = add_keeper_archiving_task(repo_id, self.user)
+        print(resp)
+        ver = resp['version']
+
+        sleep(5)
+
+        resp = check_keeper_repo_archiving_status(repo_id, self.user, 'is_snapshot_archived')
+        assert 'is_snapshot_archived' in resp and resp['is_snapshot_archived'] == 'true', 'Check response: %s' % resp
+
+        resp = query_keeper_archiving_status(repo_id, SERVER_EMAIL, ver)
+        print(resp)
 
 
-    seafile_api.post_dir(repo.id, "/", "sub_dir1", SERVER_EMAIL)
-    seafile_api.post_dir(repo.id, "/", "sub_dir2", SERVER_EMAIL)
-    seafile_api.post_dir(repo.id, "/sub_dir1", "sub_sub_dir3", SERVER_EMAIL)
 
-    f = tempfile.NamedTemporaryFile()
-    f.write('Text2')
-    f.flush()
+        #resp = query_keeper_archiving_status(repo_id, SERVER_EMAIL, resp['version'])
+        # print(resp)
 
-    seafile_api.post_file(repo.id, f.name, "/sub_dir1", "file_in_dir1.txt", SERVER_EMAIL)
-    f.close()
+        # assert 'Cannot check archiving status of the snapshot.' == resp['error']
 
-    f = tempfile.NamedTemporaryFile()
-    f.write('Text3')
-    f.flush()
-
-    seafile_api.post_file(repo.id, f.name, "/sub_dir2", "file_in_dir2.txt", SERVER_EMAIL)
-    f.close()
-
-    f = tempfile.NamedTemporaryFile()
-    f.write('Text4')
-    f.flush()
-    # os.chmod(f.name, 0666)
-
-    seafile_api.post_file(repo.id, f.name, "/sub_dir1/sub_sub_dir3", "file_in_dir3.txt", SERVER_EMAIL)
-    f.close()
-
-    arch_mgr.archive_repo(repo)
-
-    path = arch_mgr.get_archive_path(seafile_api.get_repo_owner(repo.id), repo.id, 1)
+        # resp = check_keeper_repo_archiving_status(repo.id, SERVER_EMAIL, 'get_quota')
+        # print(resp)
+        # assert 'Cannot get archiving quota.' in resp['error']
 
 
-    assert os.path.isfile(path)
-    assert os.path.getsize(path)>0
-
-    #clean up
-    # os.remove(path)
-    # shutil.rmtree(os.path.dirname(path))
+        # arch_mgr.archive_repo(repo)
+        #
+        # path = arch_mgr.get_archive_path(seafile_api.get_repo_owner(repo.id), repo.id, 1)
 
 
+        # assert os.path.isfile(path)
+        # assert os.path.getsize(path)>0
 
-@pytest.mark.skip
-def test_huge_repo_archiving():
-    """TODO: test_huge_repo_archiving
+        #clean up
+        # os.remove(path)
+        # shutil.rmtree(os.path.dirname(path))
 
-    """
-    repo_id = '8c8752f3-7f00-45ef-af9c-8f4bf29c1662'
-    repo = seafile_api.get_repo(repo_id)
-    assert not repo is None
+    @pytest.mark.skip
+    def test_tmp_repo_archiving(create_tmp_repo):
+        """TODO: Docstring for test_archiving.
 
-    start = datetime.now()
+        :create_tmp_repo: TODO
+        :returns: TODO
 
-    arch_mgr.archive_repo(repo)
+        """
+        repo = create_tmp_repo
+        assert not repo is None
 
-    print(datetime.now() - start)
+        f = tempfile.NamedTemporaryFile()
+        os.chmod(f.name, 0o777)
+        f.write(b'Text')
+        f.flush()
+        seafile_api.post_file(repo.id, f.name, "/", "some_file_in_root.txt", SERVER_EMAIL)
+        f.close()
 
-    path = arch_mgr.get_archive_path(seafile_api.get_repo_owner(repo.id), repo.id, 1)
 
-    assert os.path.isfile(path)
-    assert os.path.getsize(path)>0
+        seafile_api.post_dir(repo.id, "/", "sub_dir1", SERVER_EMAIL)
+        seafile_api.post_dir(repo.id, "/", "sub_dir2", SERVER_EMAIL)
+        seafile_api.post_dir(repo.id, "/sub_dir1", "sub_sub_dir3", SERVER_EMAIL)
 
-# @pytest.mark.skip
-def test_new_archiving(mocker):
+        f = tempfile.NamedTemporaryFile()
+        os.chmod(f.name, 0o777)
+        f.write(b'Text2')
+        f.flush()
 
-    try:
-        # config_file = get_config(os.path.join(os.environ['SEAFILE_CENTRAL_CONF_DIR'], 'seafevents.conf'))
-        # keeper_config = get_keeper_archiving_conf(config_file)
-        # for key in ('enabled', 'workers', 'archiving_storage', 'archive-max-size', 'archives-per-library',):
-            # assert key in keeper_config
+        seafile_api.post_file(repo.id, f.name, "/sub_dir1", "file_in_dir1.txt", SERVER_EMAIL)
+        f.close()
 
-        # keeper_archiving = KeeperArchiving(keeper_config)
+        f = tempfile.NamedTemporaryFile()
+        os.chmod(f.name, 0o777)
+        f.write(b'Text3')
+        f.flush()
 
-        # if keeper_archiving and keeper_archiving.is_enabled():
+        seafile_api.post_file(repo.id, f.name, "/sub_dir2", "file_in_dir2.txt", SERVER_EMAIL)
+        f.close()
+
+        f = tempfile.NamedTemporaryFile()
+        os.chmod(f.name, 0o777)
+        f.write(b'Text4')
+        f.flush()
+        # os.chmod(f.name, 0666)
+
+        seafile_api.post_file(repo.id, f.name, "/sub_dir1/sub_sub_dir3", "file_in_dir3.txt", SERVER_EMAIL)
+        f.close()
+
+        resp = check_keeper_repo_archiving_status(repo.id, SERVER_EMAIL, 'is_snapshot_archived')
+        print(resp)
+        assert 'Cannot check archiving status of the snapshot.' == resp['error']
+
+        # resp = check_keeper_repo_archiving_status(repo.id, SERVER_EMAIL, 'get_quota')
+        # print(resp)
+        # assert 'Cannot get archiving quota.' in resp['error']
+
+
+        # arch_mgr.archive_repo(repo)
+        #
+        # path = arch_mgr.get_archive_path(seafile_api.get_repo_owner(repo.id), repo.id, 1)
+
+
+        # assert os.path.isfile(path)
+        # assert os.path.getsize(path)>0
+
+        #clean up
+        # os.remove(path)
+        # shutil.rmtree(os.path.dirname(path))
+        sleep(5)
+
+    @pytest.mark.skip
+    def test_huge_repo_archiving():
+        """TODO: test_huge_repo_archiving
+
+        """
+        repo_id = '8c8752f3-7f00-45ef-af9c-8f4bf29c1662'
+        repo = seafile_api.get_repo(repo_id)
+        assert not repo is None
+
+        start = datetime.now()
+
+        arch_mgr.archive_repo(repo)
+
+        print(datetime.now() - start)
+
+        path = arch_mgr.get_archive_path(seafile_api.get_repo_owner(repo.id), repo.id, 1)
+
+        assert os.path.isfile(path)
+        assert os.path.getsize(path)>0
+
+    @pytest.mark.skip
+    def test_new_archiving(mocker):
+
+        try:
+            # config_file = get_config(os.path.join(os.environ['SEAFILE_CENTRAL_CONF_DIR'], 'seafevents.conf'))
+            # keeper_config = get_keeper_archiving_conf(config_file)
+            # for key in ('enabled', 'workers', 'archiving_storage', 'archive-max-size', 'archives-per-library',):
+                # assert key in keeper_config
+
+            # keeper_archiving = KeeperArchiving(keeper_config)
+
+            # if keeper_archiving and keeper_archiving.is_enabled():
+                # keeper_archiving.start()
+
             # keeper_archiving.start()
 
-        # keeper_archiving.start()
+            # send_mail_mock = mocker.patch('django.core.mail.message.EmailMessage.send')
+            # with mock.patch('django.core.mail.message.EmailMessage') as mocked_email:
+            # with mock.patch('seahub.utils.EmailMessage') as mocked_email:
+            # with mock.patch('post_office.models.EmailMessage') as mocked_email:
+            # with mock.patch('django.core.mail.EmailMessage') as mocked_email:
+            with mock.patch('seafevents.tasks.seahub_email_sender.SendSeahubEmailTimer') as mocked_email:
 
-        # send_mail_mock = mocker.patch('django.core.mail.message.EmailMessage.send')
-        # with mock.patch('django.core.mail.message.EmailMessage') as mocked_email:
-        # with mock.patch('seahub.utils.EmailMessage') as mocked_email:
-        # with mock.patch('post_office.models.EmailMessage') as mocked_email:
-        # with mock.patch('django.core.mail.EmailMessage') as mocked_email:
-        with mock.patch('seafevents.tasks.seahub_email_sender.SendSeahubEmailTimer') as mocked_email:
+                repo_id = '1c13f29b-f4bc-45d6-bfbb-6974320f04ec'
+                owner = 'makarenko@mpdl.mpg.de'
+                # owner = 'vlamak868@gmail.com'
 
-            repo_id = '1c13f29b-f4bc-45d6-bfbb-6974320f04ec'
-            owner = 'makarenko@mpdl.mpg.de'
-            # owner = 'vlamak868@gmail.com'
+                resp1 = add_keeper_archiving_task(repo_id, owner)
+                print(resp1.__dict__)
+                # sleep(10)
 
-            resp1 = add_keeper_archiving_task(repo_id, owner)
-            print(resp1.__dict__)
+                # repo_id = 'bdca5491-e60f-413a-af5f-33003bca2292'
+                # resp2 = add_keeper_archiving_task(repo_id, owner)
+                # print(resp2.__dict__)
+
+
+                sleep(5)
+                # resp1 = query_keeper_archiving_status(resp1.repo_id, resp1.version)
+                # print(resp1.__dict__)
+
+
+                resp = get_keeper_archiving_quota(repo_id, owner)
+                print(resp.__dict__)
+
+
+                # sleep(5)
+                # resp2 = query_keeper_archiving_status(resp2.repo_id, resp2.version)
+                # print(resp2.__dict__)
+
+                # mocked_email.send_seahub_email.assert_called()
+            # print(query_keeper_archiving_status(task2.repo_id, task2.version))
             # sleep(10)
 
-            # repo_id = 'bdca5491-e60f-413a-af5f-33003bca2292'
-            # resp2 = add_keeper_archiving_task(repo_id, owner)
-            # print(resp2.__dict__)
+            # keeper_archiving.stop()
+            # ret = query_keeper_archiving_status(repo_id)
+            # print(ret)
 
+            # print(threading.enumerate())
 
-            sleep(5)
-            # resp1 = query_keeper_archiving_status(resp1.repo_id, resp1.version)
-            # print(resp1.__dict__)
-
-
-            resp = get_keeper_archiving_quota(repo_id, owner)
-            print(resp.__dict__)
-
-
-            # sleep(5)
-            # resp2 = query_keeper_archiving_status(resp2.repo_id, resp2.version)
-            # print(resp2.__dict__)
-
-            # mocked_email.send_seahub_email.assert_called()
-        # print(query_keeper_archiving_status(task2.repo_id, task2.version))
-        # sleep(10)
-
-        # keeper_archiving.stop()
-        # ret = query_keeper_archiving_status(repo_id)
-        # print(ret)
-
-        # print(threading.enumerate())
-
-    finally:
-        pass
+        finally:
+            pass
 
 
