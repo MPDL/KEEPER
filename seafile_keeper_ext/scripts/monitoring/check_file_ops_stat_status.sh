26ba1b7e1bd325e606674189bc4d26103b7fcc31
Vlad Makarenko <makarenko@mpdl.mpg.de>
2022-03-16 15:23:10 +0100

    +icinga check_file_ops_stat_status.sh script

---
+++ seafile_keeper_ext/scripts/monitoring/check_file_ops_stat_status.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# icinga check script for KEEPER non handled viruses
+# to be copied to /usr/local/nagios/libexec
+
+# See https://github.com/MPDL/KEEPER/issues/26
+
+RESULTS=$(/opt/seafile/scripts/run_keeper_script.sh /opt/seafile/scripts/monitoring/file_ops_stat_status.py)
+RC=$?
+
+echo -e "$RESULTS"
+
+exit $RC 
--- seafile_keeper_ext/scripts/monitoring/file_ops_stat_status.py
+++ seafile_keeper_ext/scripts/monitoring/file_ops_stat_status.py
@@ -10,8 +10,8 @@ from django.db import connection
 """Get FileOpsStat status, and thus libevents correct propogation"""
 
 """
-If there are no entries in the `FileOpsStat` since 1 HOUR then most probably libevents propagatio does not work
-**NOTE:** timestamp in `FileOpsStat` saved in other timezone (from NOW() - 2 HOURS), thus add 2 HOURS. 1 HOUR is added to set time span to be in.
+If there are no entries in the `FileOpsStat` since x `HOURS` then most probably libevents propagation does not work
+**NOTE:** timestamp in `FileOpsStat` saved in other timezone (from NOW() - x - 2 `HOURS`), thus add 2 HOURS. 1 HOUR is added to set time span to be in.
 """
 
 RC = 0
@@ -19,7 +19,7 @@ RC = 0
 cur = connection.cursor()
 
 # should be increased by KEEPER growth, check `FileOpsStat` table
-HOURS = 5
+HOURS = 4
 
 cur.execute(f"SELECT MAX(timestamp) + INTERVAL {HOURS} HOUR < NOW() FROM `FileOpsStat`")
 rows = cur.fetchall()
@@ -27,8 +27,10 @@ rows = cur.fetchall()
 # for row in rows:
     # print(row)
 
-if rows[0] != 0:
-    print("No new FileOpsStat entries since {HOURS} hours. WARNING: libevents propagation probably does not work anymore.")
+# print(rows[0][0])
+
+if rows[0][0] != 0:
+    print(f"No new FileOpsStat entries since {HOURS} hour(s). WARNING: libevents propagation probably does not work anymore.")
     RC = 2
 
 sys.exit(RC)
