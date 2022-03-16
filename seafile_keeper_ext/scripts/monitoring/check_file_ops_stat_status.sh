3dcf5697065babf797747c7a6c237439a37087e7
Vlad Makarenko <makarenko@mpdl.mpg.de>
2022-03-16 15:36:51 +0100

    fixed doc in check_file_ops_stat_status.sh

--- seafile_keeper_ext/scripts/monitoring/check_file_ops_stat_status.sh
+++ seafile_keeper_ext/scripts/monitoring/check_file_ops_stat_status.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# icinga check script for KEEPER non handled viruses
-# to be copied to /usr/local/nagios/libexec
-
-# See https://github.com/MPDL/KEEPER/issues/26
+# icinga check script for KEEPER libevent propagation problem
+# to be linked from /usr/local/nagios/libexec
 
 RESULTS=$(/opt/seafile/scripts/run_keeper_script.sh /opt/seafile/scripts/monitoring/file_ops_stat_status.py)
 RC=$?
