00b1673159fcacbf6a3376ba6edacc09fb9199b4
Vlad Makarenko <makarenko@mpdl.mpg.de>
2024-02-21 13:27:35 +0100

    +generated fe

--- seafile_keeper_ext/seafile-server-latest/seahub/seahub/settings.py
+++ seafile_keeper_ext/seafile-server-latest/seahub/seahub/settings.py
@@ -695,8 +695,6 @@ SESSION_COOKIE_AGE = 24 * 60 * 60
 # Days of remembered login info (deafult: 7 days)
 LOGIN_REMEMBER_DAYS = 7
 
-SEAFILE_VERSION = '6.3.3'
-
 CAPTCHA_IMAGE_SIZE = (90, 42)
 
 ###################
