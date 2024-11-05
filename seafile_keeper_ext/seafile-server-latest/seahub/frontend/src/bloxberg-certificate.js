52575a0079f7f175c8384ac04770408bed7f4658
Vlad Makarenko <makarenko@mpdl.mpg.de>
2024-11-05 13:21:22 +0100

    fixed pdf view for bxb cert view

--- seafile_keeper_ext/seafile-server-latest/seahub/frontend/src/bloxberg-certificate.js
+++ seafile_keeper_ext/seafile-server-latest/seahub/frontend/src/bloxberg-certificate.js
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 import {
   gettext,
   siteRoot,
@@ -8,14 +8,13 @@ import {
   logoWidth,
   logoHeight,
   siteTitle,
-} from "./utils/constants";
-import PDFViewer from "./components/pdf-viewer";
-import ExpandText from "./components/expand-text";
-// import ShowMore from "react-show-more";
-import ReadMoreArea from "@foxeian/react-read-more";
+} from './utils/constants';
+import PDFViewer from './components/pdf-viewer';
+import ExpandText from './components/expand-text';
+import ReadMoreArea from '@foxeian/react-read-more';
 
-import "./css/bloxberg-certificate.css";
-import "./css/pdf-file-view.css";
+import './css/bloxberg-certificate.css';
+import './css/pdf-file-view.css';
 const {
   repoName,
   repoDesc,
@@ -31,23 +30,9 @@ const {
 class BloxbergCertificatePage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      modalIsOpen: false,
-      metadataUrl: metadataUrl,
-    };
   }
 
-  toggleModal = () => {
-    this.setState({
-      modalIsOpen: !this.state.modalIsOpen,
-      metadataUrl: "",
-    });
-  };
-
   render() {
-    let PDFViewerClassName = this.state.modalIsOpen
-      ? "file-view-content-full flex-1 pdf-file-view col-md-8 offset-md-2"
-      : "file-view-content flex-1 pdf-file-view col-md-6 offset-md-1";
     let transactionLink = "__BLOXBERG_EXPLORER__/tx/" + transactionId;
     return (
       <div className="h-100 d-flex flex-column">
@@ -63,75 +48,65 @@ class BloxbergCertificatePage extends React.Component {
           </a>
         </div>
 
-        {!this.state.modalIsOpen && (
-          <div className="container-fluid pt-4 pb-6 o-auto">
-            <div className="row">
-              <div className="col-md-6 offset-md-1 shadow">
-                <h1>
-                  <ExpandText
-                    maxLength={40}
-                    className="cert_title"
-                    text={repoName}
-                  />
-                </h1>
-                {/* <ShowMore */}
-                {/*   lines={3} */}
-                {/*   more="Show more" */}
-                {/*   less="Show less" */}
-                {/*   anchorClass="" */}
-                {/* > */}
-                {/*   {repoDesc} */}
-                {/* </ShowMore> */}
-                <ReadMoreArea
-                  letttersLimit={500}
-                  expandLabel="Read more"
-                  collapseLabel="Read less"
-                >
-                  {repoDesc}
-                </ReadMoreArea>
-                <div className="table_row">
-                  <b>{gettext("Author(s)")}: </b>
-                  {authors}
-                </div>
-                {institute && (
-                  <div className="cert_table_row">
-                    <b>{gettext("Institute")}: </b>
-                    {institute}
-                  </div>
-                )}
-                {year && (
-                  <div className="cert_table_row">
-                    <b>{gettext("Year")}: </b>
-                    {year}
-                  </div>
-                )}
-                <div className="cert_table_row">
-                  <b>{gettext("Transaction")}: </b>
-                  <a href={transactionLink} target="_blank">
-                    {transactionId}
-                  </a>
-                </div>
-                <div className="cert_table_row">
-                  <a href={historyFileUrl} download>
-                    {gettext("Download File")}
-                  </a>
-                </div>
-                <div className="cert_table_row">
-                  <a href={pdfUrl} download>
-                    {gettext("Download Certificate")}
-                  </a>
-                </div>
+        <div className="container-fluid pt-4 pb-6 o-auto">
+          <div className="row">
+            <div className="col-md-6 offset-md-1 shadow">
+              <h1>
+                <ExpandText
+                  maxLength={40}
+                  className="cert_title"
+                  text={repoName}
+                />
+              </h1>
+              <ReadMoreArea
+                lettersLimit={500}
+                expandLabel="Read more"
+                collapseLabel="Read less"
+              >
+                {repoDesc}
+              </ReadMoreArea>
+              <div className="table_row">
+                <b>{gettext('Author(s)')}: </b>
+                {authors}
               </div>
-              <div className="col-md-4">
-                <blockcerts-verifier
-                  display-mode="card"
-                  src={this.state.metadataUrl}
-                ></blockcerts-verifier>
+              {institute && (
+                <div className="cert_table_row">
+                  <b>{gettext('Institute')}: </b>
+                  {institute}
+                </div>
+              )}
+              {year && (
+                <div className="cert_table_row">
+                  <b>{gettext('Year')}: </b>
+                  {year}
+                </div>
+              )}
+              <div className="cert_table_row">
+                <b>{gettext('Transaction')}: </b>
+                <a href={transactionLink} target="_blank" rel="noreferrer">
+                  {transactionId}
+                </a>
+              </div>
+              <div className="cert_table_row">
+                <a href={historyFileUrl} download>
+                  {gettext('Download File')}
+                </a>
+              </div>
+              <div className="cert_table_row">
+                <a href={pdfUrl} download>
+                  {gettext('Download Certificate')}
+                </a>
               </div>
             </div>
+            <div className="col-md-4">
+              <blockcerts-verifier
+                display-mode="card"
+                src={metadataUrl}
+              ></blockcerts-verifier>
+            </div>
           </div>
-        )}
-        <div className={PDFViewerClassName} onClick={this.toggleModal}>
+        </div>
+        <div className="file-view-content flex-1 pdf-file-view">
           <PDFViewer />
         </div>
       </div>
@@ -141,5 +116,5 @@ class BloxbergCertificatePage extends React.Component {
 
 ReactDOM.render(
   <BloxbergCertificatePage />,
-  document.getElementById("wrapper")
+  document.getElementById('wrapper')
 );
