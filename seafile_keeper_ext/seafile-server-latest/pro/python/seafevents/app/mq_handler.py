77e2147e2d653558d63e496ba8840d9febaa7e25
Iosif Peterfi <peterfi@mpdl.mpg.de>
2026-02-04 13:29:37 +0100

    Update mq_handler.py
    Removed the debugging which was preventing events from being propagated correctly.

--- seafile_keeper_ext/seafile-server-latest/pro/python/seafevents/app/mq_handler.py
+++ seafile_keeper_ext/seafile-server-latest/pro/python/seafevents/app/mq_handler.py
@@ -1,10 +1,7 @@
 import time
 import logging
 import json
-from threading import Thread, excepthook
-import threading
-import traceback
-import sys
+from threading import Thread
 
 from seaserv import seafile_api
 
@@ -17,20 +14,10 @@ import seafevents.repo_metadata.handlers as metadata_handler
 
 logger = logging.getLogger(__name__)
 
-from datetime import datetime, timedelta
-k_log = logging.getLogger('keeper_debug')
-
 __all__ = [
     'EventsHandler',
     'init_message_handlers'
 ]
-
-# KEEPER: add exception hook on any Thread
-def handle_thread_exception(args):
-    exc_msg = traceback.format_exc()
-    k_log.debug("Exception in thread %s: %s, %s, error: %s", args.thread.name, args.exc_type, args.exc_value, exc_msg)
-
-threading.excepthook = handle_thread_exception
 
 
 class MessageHandler(object):
@@ -111,56 +98,33 @@ class EventsHandler(object):
         self._config = config
         self._db_session_class = init_db_session_class(config)
         self._redis_connection = RedisClient(config).connection
-        self._past_ts_dict =  {}
-        self._counter = 0
 
     def handle_event(self, channel):
         config = self._config
         session = self._db_session_class()
         redis_connection = self._redis_connection
-        try:
-            k_log.debug('Starting handle_event thread for %s channel...', channel)
-            while 1:
+        while 1:
+            try:
+                msg = seafile_api.pop_event(channel)
+            except Exception as e:
+                logger.error('Failed to get event: %s' % e)
+                time.sleep(3)
+                continue
+            if msg:
                 try:
-                    curr_ts = datetime.now()
-                    if curr_ts - self._past_ts_dict[channel] >= timedelta(minutes=1):
-                        k_log.debug('Channel %s thread is alive', channel)
-                        self._past_ts_dict[channel] = curr_ts
-                    msg = seafile_api.pop_event(channel)
+                    message_handler.handle_message(config, session, redis_connection, channel, msg)
                 except Exception as e:
-                    logger.error('Failed to get event: %s' % e)
-                    k_log.debug('Failed to get event in channel %s: %s', channel, e)
-                    time.sleep(3)
-                    continue
-                if channel == 'seaf_server.event':
-                    k_log.debug('Counter: %s', self._counter)
-                    # if self._counter == 3:
-                    #     sys.exit(1)
-                    # self._counter += 1
-                if msg:
-                    if channel == 'seaf_server.event':
-                        k_log.debug(msg)
-                        try:
-                            message_handler.handle_message(config, session, redis_connection, channel, msg)
-                        except Exception as e:
-                            logger.error(e)
-                            k_log.debug('Failed to handle_message in channel %s: %s', channel, e)
-                        finally:
-                            session.close()
-                            if redis_connection:
-                                redis_connection.close()
-                else:
-                    time.sleep(0.5)
-        except SystemExit as sa:
-            raise Exception("Catched SystemExit exception", sa)
-        finally:
-            k_log.debug('Leaving channel %s thread...', channel)
-
+                    logger.error(e)
+                finally:
+                    session.close()
+                    if redis_connection:
+                        redis_connection.close()
+            else:
+                time.sleep(0.5)
 
     def start(self):
         channels = message_handler.get_channels()
         logger.info('Subscribe to channels: %s', channels)
         for channel in channels:
-            self._past_ts_dict[channel] = datetime.now()
             event_handler = Thread(target=self.handle_event, args=(channel, ))
             event_handler.start()
